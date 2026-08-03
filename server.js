4be1ed660887e8879f39de6303d5f1c885d28261
Tim <147756193+zireael06@users.noreply.github.com>
2026-08-04 00:22:42 +0100

    Prepare app for Render deployment

--- server.js
+++ server.js
@@ -8,11 +8,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const db = new pg.Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 db.connect()
